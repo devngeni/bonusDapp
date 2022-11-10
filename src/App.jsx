967da95c6e5b6aa7e8b6ec0b042953c80eece706
henrykash <henrykariuki003@gmail.com>
2022-11-10 16:14:38 +0300

    custom esllet connect to bsc

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,36 @@ import { publicProvider } from "wagmi/providers/public";
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
 import { SnackbarProvider } from "notistack";
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
 import { config } from 'dotenv';
 
 
+const bscChain = {
+  id: 97,
+  name: 'Binance Smart Chain',
+  network: 'bsc',
+  iconUrl: 'https://icons8.com/icon/63192/bitcoin',
+  iconBackground: '#fff',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Binance Coin',
+    symbol: 'WBNB',
+  },
+  rpcUrls: {
+    default: 'https://bsc-dataseed4.binance.org/',
+  },
+  blockExplorers: {
+    default: { name: 'BscScan', url: 'https://www.bscscan.com/' },
+    bsc: { name: 'BscScan', url: 'https://www.bscscan.com/' },
+  },
+  testnet: false,
+};
 
-const { chains, provider } = configureChains(
-  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum, chain.goerli],
-  [alchemyProvider({ apiKey: config.REACT_APP_ALCHEMY_ID }), publicProvider()]
+const { provider, chains } = configureChains(
+  [bscChain],
+  [jsonRpcProvider({ rpc: chain => ({ http: chain.rpcUrls.default }) })]
 );
+
 
 const { connectors } = getDefaultWallets({
   appName: "THUNDER SWAP",
